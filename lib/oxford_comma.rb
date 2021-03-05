268dead95bcf9ecedac61f2f50c9d294f6e17c79
alexandru <boncut.alex10@gmail.com>
2021-03-04 18:24:58 -0600

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,13 @@
 def oxford_comma(array)
-
+  array_final = ""
+  if array.length == 1
+    array_final = array.join
+  elsif array.length == 2
+    array_final = array.join(" and ")
+  elsif array.length == 3
+    array_final = array[0..1].join(", ") << " and " << array[-1]
+  else
+    array_final = array[0..-1].join(", ") << " and " << array[-1]
+  end
+  return array_final
 end
